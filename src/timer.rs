409aab7e8a144b6518f94d0f04341f13a3d0eb7c
vuonojenmustaturska <naksu@youzen.ext.b2.fi>
2021-03-14 16:02:04 +0200

    wip2: electric boogaloo

--- src/timer.rs
+++ src/timer.rs
@@ -1,57 +1,76 @@
-use std::time::Duration;
-use std::thread;
-
-use pendulum::{Pendulum, HashedWheelBuilder, HashedWheel};
-
-use std::collections::HashMap;
 use std::cell::RefCell;
+use std::time::{Instant, Duration};
+use ferris::{AllocWheel, Resolution, Wheel};
 
 thread_local! {
-    static REALTIMERS: RefCell<HashedWheel<String>> = RefCell::new(HashedWheelBuilder::default().with_tick_duration(Duration::from_millis(100)).build::<String>());
-    static BYONDTIMERS: RefCell<HashedWheel<String>> = RefCell::new(HashedWheelBuilder::default().with_tick_duration(Duration::from_millis(100)).build::<String>());
-    static TIMERENTRIES: RefCell<HashMap::<String, pendulum::Token>> = RefCell::new(HashMap::<String, pendulum::Token>::new());
+    static TIMERS: RefCell<Timers> = Default::default();
 }
 
 struct Timers {
-    realwheel: HashedWheel<String>,
-    byondwheel: HashedWheel<String>,
-    entries: HashMap<String, pendulum::Token>
+    realwheel: AllocWheel<String>,
+    byondwheel: AllocWheel<String>,
+    lastrun: Instant
 }
 
-byond_fn! { add_realtime_timer(timerid, delay) {
-    REALTIMERS.with(|timers|
-        TIMERENTRIES.with(|entries|
-            entries.borrow_mut().insert(timerid.to_string(), timers.borrow_mut().insert_timeout(Duration::from_millis(100 * delay.parse::<u64>().unwrap()), timerid.to_string()).unwrap()))
+impl Default for Timers {
+    fn default() -> Self {
+        let mut resolutions1 = Vec::<Resolution>::new();
+        let mut resolutions2 = Vec::<Resolution>::new();
+        resolutions1.push(Resolution::HundredMs);
+        resolutions1.push(Resolution::Sec);
+        resolutions1.push(Resolution::Min); // = vec!([, Resolution::Sec, Resolution::Min]);
+        resolutions2.push(Resolution::HundredMs);
+        resolutions2.push(Resolution::Sec);
+        resolutions2.push(Resolution::Min); // = vec!([, Resolution::Sec, Resolution::Min]);
+        Timers { realwheel: AllocWheel::<String>::new(resolutions1),
+        byondwheel: AllocWheel::<String>::new(resolutions2),
+        lastrun: Instant::now() }
+    }
+}
+
+byond_fn! { setup_timers() {
+    TIMERS.with(|timers|
+        *timers.borrow_mut() = Default::default()
     );
     Some("")
+}}
+
+byond_fn! { add_realtime_timer(timerid, delay) {
+    match delay.parse::<u64>() {
+        Ok(duration) => { TIMERS.with(|timers| timers.borrow_mut().realwheel.start(timerid.to_owned(), Duration::from_millis(100 * duration))); Some("") },
+        Err(_e) => Some("parse error")
+    }
 } }
 
 byond_fn! { add_byondtime_timer(timerid, delay) {
-    BYONDTIMERS.with(|timers|
-        TIMERENTRIES.with(|entries|
-            entries.borrow_mut().insert(timerid.to_string(), timers.borrow_mut().insert_timeout(Duration::from_millis(100 * delay.parse::<u64>().unwrap()), timerid.to_string()).unwrap()))
+    match delay.parse::<u64>() {
+        Ok(duration) => { TIMERS.with(|timers| timers.borrow_mut().byondwheel.start(timerid.to_owned(), Duration::from_millis(100 * duration))); Some("") },
+        Err(_e) => Some("parse error")
+    }
+} }
+
+byond_fn! { del_timer(timerid) {
+    TIMERS.with(|timers|
+        timers.borrow_mut().byondwheel.stop(timerid.to_owned())
     );
     Some("")
 } }
 
-byond_fn! { del_timer(timerid) {
-    REALTIMERS.with(|timers|
-        TIMERENTRIES.with(|entries|
-        timers.borrow_mut().remove_timeout(entries.borrow_mut().remove(timerid).unwrap())
-    ))
-} }
-
-byond_fn! { get_timers(byondtick) {
+byond_fn! { get_timers() {
     let mut vec = Vec::<String>::new();
-    let byondtick = byondtick.parse::<u64>().unwrap();
-    REALTIMERS.with(|timers|
-    TIMERENTRIES.with(|entries|
-        while ()
-        while let Some(expired) = timers.borrow_mut().expired_timeout() {
-            vec.push(expired.clone());
-            entries.borrow_mut().remove(&expired);
+
+    TIMERS.with(|timers|
+        {
+        let mut timers_mut = timers.borrow_mut();
+        vec.append(&mut timers_mut.byondwheel.expire());
+        let now = Instant::now();
+        let mut diffms = now.duration_since(timers_mut.lastrun).as_millis();
+        while diffms >= 100 {
+            vec.append(&mut timers_mut.realwheel.expire());
+            diffms -= 100;
         }
-    )
+        timers_mut.lastrun = now;
+        }
     );
     Some(vec.join(","))
 }  }
